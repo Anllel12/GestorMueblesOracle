3e8391739ba00a5ee31fb6f33d7361e4b3912109
Angel Esquinas <anllelonair@gmail.com>
2021-02-26 17:44:31 +0100

    Añadido el SQL

--- sql/Muebles.sql
+++ sql/Muebles.sql
@@ -1,50 +1,90 @@
-CREATE DATABASE IF NOT EXISTS muebles;
+CREATE OR REPLACE TYPE t_mueble AS OBJECT (
+    modelo INT(10),
+	nombre VARCHAR2(100),
+	precio INT(10),
+	paquetes INT(5),
+	cantidad INT(5)
+	);
 
-USE muebles;
+/
 
-CREATE TABLE IF NOT EXISTS `mueble` (
-	`modelo` INT(10) NOT NULL,
-	`nombre` VARCHAR(100) NOT NULL,
-	`precio` INT(10) NOT NULL,
-	`paquetes` INT(5) NOT NULL,
-	PRIMARY KEY (`modelo`)
-);
+CREATE OR REPLACE TYPE t_tamano AS OBJECT (
+	id INT(10),
+	ancho INT(10),
+	fondo INT(10),
+    altura INT(10),
+	peso_balda INT(10),
+	mueble REF t_mueble
+	);
 
-CREATE TABLE IF NOT EXISTS `tamano` (
-	`id` INT(10) AUTO_INCREMENT,
-	`ancho` INT(10),
-	`fondo` INT(10),
-    `altura` INT(10),
-	`peso_balda` INT(10),
-	`mueble` INT(10) NOT NULL,
-	PRIMARY KEY (`id`),
-    FOREIGN KEY(`mueble`) REFERENCES mueble(modelo) ON DELETE CASCADE
-);
+/
 
-CREATE TABLE IF NOT EXISTS `material` (
-	`id` INT(10) AUTO_INCREMENT,
-	`principal` VARCHAR(100),
-	`secundario` VARCHAR(100),
-	`mueble` INT(10) NOT NULL,
-	PRIMARY KEY (`id`),
-    FOREIGN KEY(`mueble`) REFERENCES mueble(modelo) ON DELETE CASCADE
-    );
+CREATE OR REPLACE TYPE t_material AS OBJECT (
+	id INT(10),
+	principal VARCHAR(100),
+	secundario VARCHAR(100),
+	mueble REF t_mueble
+	);
 
-INSERT IGNORE INTO `mueble` VALUES 
-('00278578', 'HYLLIS', '10', '1'),
-('60333850', 'BROR', '99', '1'),
-('30409295', 'KOLBJÖRN', '69', '1');
+/
 
-INSERT IGNORE INTO `tamano` VALUES 
-(0, '60', '27', '140', '25', '00278578'),
-(0, '85', '55', '88', '50', '60333850'),
-(0, '80', '35', '81', '55', '30409295');
+CREATE TABLE mueble OF t_mueble;
 
-INSERT IGNORE INTO `material` VALUES 
-(0, 'Acero galvanizado', 'Plástico amídico', '00278578'),
-(0, 'Acero', 'Contrachapado de pino', '60333850'),
-(0, 'Acero galvanizado', 'Revestimiento en polvo de poliéster', '30409295');
+/
 
-SELECT * FROM mueble;
-SELECT * FROM tamano;
-SELECT * FROM material;
+CREATE TABLE tamano OF t_tamano;
+
+/
+
+CREATE TABLE material OF t_material;
+
+/
+
+INSERT INTO mueble VALUES
+	('00278578', 'HYLLIS', '10', '1', '1');
+   
+/
+
+INSERT INTO mueble VALUES
+	('60333850', 'BROR', '99', '1', '1');
+   
+/
+
+INSERT INTO mueble VALUES
+	('30409295', 'KOLBJÖRN', '69', '1', '1');
+   
+/
+
+INSERT INTO tamano VALUES
+	(1, '60', '27', '140', '25', (SELECT REF(m) FROM mueble m WHERE m.modelo = '00278578'));
+
+/
+
+INSERT INTO tamano VALUES
+	(2, '85', '55', '88', '50', (SELECT REF(m) FROM mueble m WHERE m.modelo = '60333850'));
+
+/
+
+INSERT INTO tamano VALUES
+	(3, '80', '35', '81', '55', (SELECT REF(m) FROM mueble m WHERE m.modelo = '30409295'));
+   
+/
+
+INSERT INTO material VALUES
+	(1, 'Acero galvanizado', 'Plástico amídico', (SELECT REF(m) FROM mueble m WHERE m.modelo = '00278578'));
+
+/
+
+INSERT INTO material VALUES
+	(2, 'Acero', 'Contrachapado de pino', (SELECT REF(m) FROM mueble m WHERE m.modelo = '60333850'));
+
+/
+
+INSERT INTO material VALUES
+	(3, 'Acero galvanizado', 'Revestimiento en polvo de poliéster', (SELECT REF(m) FROM mueble m WHERE m.modelo = '30409295'));
+   
+/
+
+SELECT * FROM mueble m;
+SELECT t.id, t.ancho, t.fondo, t.altura, t.peso_balda, t.mueble.modelo FROM tamano t;
+SELECT m.id, m.principal, m.secundario, m.mueble.modelo FROM material m;
